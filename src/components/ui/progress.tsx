954d93dff6b2895cd5617d8869b93b94d5af9360
mvluz <marcus.i.t@outlook.com>
2023-10-16 08:56:11 -0300

    :sparkles: Perfecting our File Uploader

--- src/components/ui/progress.tsx
+++ src/components/ui/progress.tsx
@@ -5,10 +5,12 @@ import * as ProgressPrimitive from "@radix-ui/react-progress"
 
 import { classNamesMerge } from "@/lib/utils"
 
+type ProgreeProps = React.ComponentPropsWithoutRef<typeof ProgressPrimitive.Root> & {indicatorColor?: string}
+
 const Progress = React.forwardRef<
   React.ElementRef<typeof ProgressPrimitive.Root>,
-  React.ComponentPropsWithoutRef<typeof ProgressPrimitive.Root>
->(({ className, value, ...props }, ref) => (
+  ProgreeProps
+>(({ className, value, indicatorColor, ...props }, ref) => (
   <ProgressPrimitive.Root
     ref={ref}
     className={classNamesMerge(
@@ -18,7 +20,7 @@ const Progress = React.forwardRef<
     {...props}
   >
     <ProgressPrimitive.Indicator
-      className="h-full w-full flex-1 bg-primary transition-all"
+      className={classNamesMerge("h-full w-full flex-1 bg-primary transition-all",indicatorColor)}
       style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
     />
   </ProgressPrimitive.Root>
