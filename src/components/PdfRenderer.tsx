4d6a4253630ee09c970a2a7576cd6c218ae92e61
mvluz <marcus.i.t@outlook.com>
2023-10-19 21:08:56 -0300

    :page_facing_up: Rendering PDF Files

--- src/components/PdfRenderer.tsx
+++ src/components/PdfRenderer.tsx
@@ -1,12 +1,67 @@
 'use client'
 
-const PdfRenderer = () => {
+import {
+    ChevronDown,
+    ChevronUp,
+    Loader2,
+    RotateCw,
+    Search,
+} from 'lucide-react'
+import { Document, Page, pdfjs } from 'react-pdf'
+
+import 'react-pdf/dist/Page/AnnotationLayer.css'
+import 'react-pdf/dist/Page/TextLayer.css'
+import { useToast } from './ui/use-toast'
+
+import { useResizeDetector } from 'react-resize-detector'
+import { Button } from './ui/button'
+/* import { Input } from './ui/input' */
+import { useState } from 'react'
+
+/* import { useForm } from 'react-hook-form' */
+import { z } from 'zod'
+
+/* import { zodResolver } from '@hookform/resolvers/zod' */
+import { classNamesMerge } from '@/lib/utils'
+
+
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`
+
+interface PdfRendererProps {
+    url: string
+}
+
+const PdfRenderer = ({ url }: PdfRendererProps) => {
+    const { toast } = useToast()
+    const { width, ref } = useResizeDetector()
 
     return (
         <div className='w-full bg-white rounded-md shadow flex flex-col items-center'>
             <div className='h-14 w-full border-b border-zinc-200 flex items-center justify-between px-2'>
                 <div className='flex items-center gap-1.5'>
-                    
+
+                </div>
+            </div>
+
+            <div className='flex-1 w-full max-h-screen'>
+                <div ref={ref}>
+                    <Document
+                        loading={
+                            <div className='flex justify-center'>
+                                <Loader2 className='my-24 h-6 w-6 animate-spin' />
+                            </div>
+                        }
+                        onLoadError={() => {
+                            toast({
+                                title: 'Error loading PDF',
+                                description: 'Please try again later',
+                                variant: 'destructive',
+                            })
+                        }}
+                        file={url}
+                        className='max-h-full'>
+                        <Page width={width ? width : 1} pageNumber={1}/>
+                    </Document>
                 </div>
             </div>
         </div>
